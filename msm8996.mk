6d1e793653e42aab6f52bf8f3ee8d199b55b2636
Eliminater74 <eliminater74@gmail.com>
2018-08-23 18:05:45 -0400

    [power] Drop in Google Pixel XL's (Marlin) Power Hal v1.1: Oreo 8.1_r43:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -277,6 +277,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.1-service.marlin \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.msm8996
