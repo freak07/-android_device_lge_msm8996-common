edf2fef94b366f7607d2f94f1fb80a27b087c6ab
Eliminater74 <eliminater74@gmail.com>
2018-08-27 12:22:17 -0400

    [thermal-engine] Add thermal-engine-8996.conf:
    *** This was left out when the Marlin thermal was introduced
    *** To the lge.msm8996 Tree:
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -281,6 +281,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.msm8996
+    
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
