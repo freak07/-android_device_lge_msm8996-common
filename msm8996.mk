b436a32c4e6cb14a260dae36f78bd99a38d8d27e
Eliminater74 <eliminater74@gmail.com>
2018-08-29 10:45:03 -0400

    [thermal-engine] MSM8996 thermal HAL: move to thermal-engine: (1/2)
    *** Remove passthrough libhardware-compatible Thermal HAL.
    
    *** Allow thermal-engine to serve Thermal HAL.
    
    ** Vendor Commit is Needed too:
    Commit: https://github.com/DevG6/proprietary_vendor_lge/commit/9940c55beb65198451328d01fac78d8ee057d997
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -278,9 +278,6 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service.lge.msm8996 \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8996
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
