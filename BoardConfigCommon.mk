ad13070b7bec24c980ffecf5941bdc7a75e5ba6e
Eliminater74 <eliminater74@gmail.com>
2018-08-30 18:28:49 -0400

    [BoardConfigCommin] RIL: use CAF Variant and PROTOBUF_SUPPORTED:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -202,6 +202,10 @@ BOARD_USES_QC_TIME_SERVICES := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
+# RIL
+TARGET_RIL_VARIANT := caf
+PROTOBUF_SUPPORTED := true
+
 # SELinux policies
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
