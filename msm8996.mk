194d7ed2b530561b3a2bb3eeca52c915ec5e0056
Eliminater74 <eliminater74@gmail.com>
2018-09-01 09:08:47 -0400

    [audio] Add Pro Audio Features:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 
 -include $(LOCAL_PATH)/audio/config.mk
 
+# Pro audio feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
